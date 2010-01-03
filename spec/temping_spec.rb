3e38d57b2a17f84123dc04f0102728f76855c34b
John Pignata <john.pignata@gmail.com>
2010-01-03 13:38:42 -0500

    General spec clean-up
    Add assertions for better coverage
    
    Use more generic examples
    
    Singularize symbols passed into create_model

--- spec/temping_spec.rb
+++ spec/temping_spec.rb
@@ -5,12 +5,14 @@ describe Temping do
 
   describe ".create_model" do    
     it "creates and returns an AR::Base-derived model" do
-      posts = create_model :posts
-      posts.ancestors.should include(ActiveRecord::Base)
+      post_class = create_model :post
+      post_class.ancestors.should include(ActiveRecord::Base)
+      post_class.should == Post
+      post_class.table_name.should == "posts"
     end
 
     it "evals all statements passed in through a block" do
-      create_model :votes do
+      create_model :vote do
         with_columns do |table|
           table.string :voter
         end
@@ -21,17 +23,22 @@ describe Temping do
       vote = Vote.new
       vote.should_not be_valid
       
-      vote.voter = "John Pignata"
+      vote.voter = "Foo Bar"
       vote.should be_valid
     end
     
-    it "silently skip initialization if a const is already defined" do
-      lambda { 2.times { create_model :dogs }}.should_not raise_error
+    it "silently skips initialization if a const is already defined" do
+      lambda { 2.times { create_model :dog } }.should_not raise_error
+    end
+    
+    it "returns nil if a const is already defined" do
+      create_model :cat
+      create_model(:cat).should be_nil
     end
     
     describe ".with_columns" do
       it "creates columns passed in through a block" do
-        create_model :comments do 
+        create_model :comment do 
           with_columns do |table|
             table.integer :count
             table.string :headline
@@ -47,17 +54,26 @@ describe Temping do
   describe "database agnostism" do
     it "supports Sqlite3" do
       ActiveRecord::Base.establish_connection 'temping'
-      create_model(:oranges).should == Orange
+      create_model(:orange).should == Orange
+
+      Orange.connection.class.should == ActiveRecord::ConnectionAdapters::SQLite3Adapter
+      Orange.table_name.should == "oranges"
     end
 
     it "supports MySQL" do
       ActiveRecord::Base.establish_connection 'mysql'
-      create_model(:apples).should == Apple
+      create_model(:apple).should == Apple
+
+      Apple.connection.class.should == ActiveRecord::ConnectionAdapters::MysqlAdapter
+      Apple.table_name.should == "apples"
     end
 
     it "supports PostgreSQL" do
       ActiveRecord::Base.establish_connection 'postgres'
-      create_model(:cherries).should == Cherry
+      create_model(:cherry).should == Cherry
+
+      Cherry.connection.class.should == ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
+      Cherry.table_name.should == "cherries"
     end
   end
 end
