5da621daca20dd043c249927b5895cb4ea368995
Esteban Pastorino <ejpastorino@gmail.com>
2015-03-20 16:47:53 -0400

    update gemfile.lock and sqlite connection

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,4 +3,4 @@ $: << File.join(File.dirname(__FILE__), "/../lib")
 require "bundler/setup"
 require "temping"
 
-ActiveRecord::Base.establish_connection("sqlite3:///:memory:")
+ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: ":memory:")
